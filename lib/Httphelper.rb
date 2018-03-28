6ef860a580cc7ab0bc4a69ac77c681509132d971
andersonericw <anderson.ericw@gmail.com>
2018-03-28 15:48:56 -0400

    Update Httphelper.rb
    removed stupid, dated code.

--- lib/Httphelper.rb
+++ lib/Httphelper.rb
@@ -1,10 +1,6 @@
 require 'net/http'
 class Net::HTTPResponse
   def success?
-    if ! self.is_a? Net::HTTPResponse
-      raise 'http_response must be Net::HTTP'
-      return nil
-    end
     return false if ! self.is_a? Net::HTTPSuccess
     return true
   end
